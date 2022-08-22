94db244e7338021e619d6e19d23966cd812b3067
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-22 23:45:59 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+def initialize(word)
+    @word = word
+end
+
+def match(words_collection)
+    matched_words = []
+    words_collection.each do |item|
+        if @word.chars.sort == item.chars.sort
+            matched_words << item
+        end
+    end
+    matched_words
+end
+end
+
